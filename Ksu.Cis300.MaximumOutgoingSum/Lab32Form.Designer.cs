62055c2660f34db12ce2c2102e9e949d072e31ef
NickRuffini <njruffini@gmail.com>
2020-04-15 09:42:25 -0500

    Lab 32 Complete

---
+++ Ksu.Cis300.MaximumOutgoingSum/Lab32Form.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Ksu.Cis300.MaximumOutgoingSum
+{
+    partial class Lab32Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxButton = new System.Windows.Forms.Button();
+            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // uxButton
+            // 
+            this.uxButton.Location = new System.Drawing.Point(46, 84);
+            this.uxButton.Name = "uxButton";
+            this.uxButton.Size = new System.Drawing.Size(187, 66);
+            this.uxButton.TabIndex = 0;
+            this.uxButton.Text = "Read a Graph";
+            this.uxButton.UseVisualStyleBackColor = true;
+            this.uxButton.Click += new System.EventHandler(this.uxButton_Click);
+            // 
+            // uxOpenFileDialog
+            // 
+            this.uxOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // Lab32Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 229);
+            this.Controls.Add(this.uxButton);
+            this.Name = "Lab32Form";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uxButton;
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
+    }
+}
+
---
+++ Ksu.Cis300.MaximumOutgoingSum/Lab32Form.cs
@@ -0,0 +1,95 @@
+﻿/* Lab32Form.cs
+ * Author: Nick Ruffini
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Ksu.Cis300.Graphs;
+using System.IO;
+
+namespace Ksu.Cis300.MaximumOutgoingSum
+{
+    public partial class Lab32Form : Form
+    {
+        public Lab32Form()
+        {
+            InitializeComponent();
+        }
+
+        private void uxButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (uxOpenFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = uxOpenFileDialog.FileName;
+                    DirectedGraph<string, decimal> graph = ReadGraph(filePath);
+                    decimal maxSum = MaxSum(graph);
+                    MessageBox.Show(maxSum.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            
+        }
+
+        /// <summary>
+        /// Reads a graph in from a file!
+        /// </summary>
+        /// <param name="fileName"> Name of the file we are reading in </param>
+        /// <returns> DirectedGraph containing the graph we just read in </returns>
+        private DirectedGraph<string, decimal> ReadGraph(string fileName)
+        {
+            DirectedGraph<string, decimal> newGraph = new DirectedGraph<string, decimal>();
+
+            using (StreamReader sr = new StreamReader(fileName))
+            {
+                sr.ReadLine();
+                string line;
+                while((line = sr.ReadLine()) != null)
+                {
+                    string[] components = line.Split(new Char[] {','});
+                    string sourceNode = components[0];
+                    string destinationNode = components[1];
+                    decimal weight = Convert.ToDecimal(components[2]);
+
+                    newGraph.AddEdge(sourceNode, destinationNode, weight);
+                }
+            }
+            return newGraph;
+        }
+
+        /// <summary>
+        ///  Find the maximum sum of edge weights from any node in a graph
+        /// </summary>
+        /// <param name="graph"> Graph that we are traversing through </param>
+        /// <returns> Decimal value of the max sum of a node's edges from our graph </returns>
+        private decimal MaxSum(DirectedGraph<string, decimal> graph)
+        {
+            decimal max = 0;
+            foreach (string node in graph.Nodes)
+            {
+                decimal thisNodeSum = 0;
+                foreach(Edge<string, decimal> value in graph.OutgoingEdges(node))
+                {
+                    thisNodeSum += value.Data;
+                }
+                if (thisNodeSum > max)
+                {
+                    max = thisNodeSum;
+                }
+            }
+            return max;
+        }
+    }
+}
